55d0c7a2fe195e0c5ade6523453dc545cf5aa147
Robert Macias <rsmacias@gmail.com>
2022-11-13 22:46:04 -0500

    New initialization

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!!!
-        </p>
-      </header>
-    </div>
+    <main>
+      <p>
+        Hello World!!!
+      </p>
+    </main>
   );
 }
 
