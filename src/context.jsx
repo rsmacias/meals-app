a66841843f4453ce4702b61a11bfdae4cb204cd1
Robert Macias <rsmacias@gmail.com>
2022-11-20 13:08:15 -0500

    My approach of the challenge.

--- src/context.jsx
+++ src/context.jsx
@@ -4,18 +4,47 @@ const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
 
+    const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php';
+    const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+    const getMealsByName = async (name) => {
+        try {
+            const response = await fetch(`${allMealsUrl}?s=${name}`);
+            const data = await response.json();
+            //console.log(data.meals[0]);
+            return data.meals[0];
+        } catch(error) {
+            console.log(error);
+            return null;
+        }
+    };
+
+    const getRandomMeal = async () => {
+        try {
+            const response = await fetch(randomMealUrl);
+            const data = await response.json();
+            //console.log(data);
+            return data.meals[0];
+        } catch(error) {
+            console.log(error);
+            return null;
+        }
+    };
+
     const fetchData = async () => {
         try {
             const response = await fetch('https://randomuser.me/api/');
             const data = await response.json();
-            console.log(data);
+            //console.log(data);
         } catch(error) {
             console.log(error);
         }
     };
 
     useEffect(() => {
-        fetchData();
+        console.log('fetchData', fetchData());
+        console.log('getRandomMeal', getRandomMeal());
+        console.log('getMealsByName', getMealsByName('Arrabiata'));
     }, []);
 
     return (
