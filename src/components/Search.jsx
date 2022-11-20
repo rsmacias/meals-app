ea7e81bbcdede5155c6fa161cd2959563e180724
Robert Macias <rsmacias@gmail.com>
2022-11-20 16:51:23 -0500

    Structure search behavior for Search component.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,11 +4,25 @@ import { useGlobalContext } from "../context";
 const Search = () => {
 
     const context = useGlobalContext();
+    const [text, setText] = useState('');
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('search for ...', text);
+    }
 
     return (
         <header className="search-container">
-            <form action="">
-                <input type="text" className="form-input" placeholder="type favorite meal" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" 
+                       className="form-input" 
+                       placeholder="type favorite meal"
+                       onChange={handleChange}
+                       value={text} />
                 <button type="submit" className="btn">Search</button>
                 <button type="button" className="btn btn-hipster">Surprise me!</button>
             </form>
