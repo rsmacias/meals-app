e9020c8a1b5994118bd95c821d89a7ec9c322c89
Robert Macias <rsmacias@gmail.com>
2022-11-20 12:36:10 -0500

    Fetching data: Approach #1 with async/await - Fetch function inside useEffect hook

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,22 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://randomuser.me/api/');
+                const data = await response.json();
+                console.log(data);
+            } catch(error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <AppContext.Provider value="hello">
             {children}
